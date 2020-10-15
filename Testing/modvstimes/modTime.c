40b3011ad9bfbac670f1b108c50ebce33628dbcd
Matthew Ouyang <wouy448@aucklanduni.ac.nz>
2020-10-16 11:06:38 +1300

    testing study b4 test

--- Testing/modvstimes/modTime.c
+++ Testing/modvstimes/modTime.c
@@ -1,10 +1,21 @@
 #include<stdio.h>
 
+#define A 1 + 2
+#define B 5
+#define C 2 * 3
+
 int main(void) 
 {
-    int i;
-    i = 4%5;
-    // multiplication > mod
-    printf("%d", i);
+    int i, j;
+    double k;
+    i = 4+5%3;
+    j = (int) 1.9999;
+    k = 4/3;
+
+    int x = A*B;
+    int y = B+C;
+
+    printf("%d %d %f\n", i, j, k);
+    printf("%d %d", x, y);
     return 0;
 }
